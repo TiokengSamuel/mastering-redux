718a4e6218326b0e64a1bebe79de8afeb46c3692
TiokengSamuel <samueledward40@gmail.com>
2020-11-04 07:35:53 +0100

    app

--- demo/src/App.js
+++ demo/src/App.js
@@ -2,9 +2,14 @@ import React from 'react'
 import './App.css';
 
 function App() {
+  const [user, setUser] = useState(null);
   return (
     <div className="App">
       Showing the power of redux with facebook app
+
+      <Login/>
+      <Header/>
+      <Feed/>
     </div>
   );
 }
