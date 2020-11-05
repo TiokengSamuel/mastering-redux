a43bdbc7b9fd9c152d9eb815601ad4e238536be4
TiokengSamuel <samueledward40@gmail.com>
2020-11-05 16:49:58 +0100

    wrapping with provider

--- context-app/src/App.js
+++ context-app/src/App.js
@@ -2,12 +2,16 @@
   import React from 'react'
 import BookList from './components/BookList'
 import Navbar from './components/Navbar'
+import ThemeContextProvider from './context/ThemeContext'
   
   function App() {
     return (
       <div className="App">
+        <ThemeContextProvider>
           <Navbar/>
           <BookList/>
+        </ThemeContextProvider>
+          
       </div>
     )
   }
