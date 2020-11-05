a350f434fe7e7948653075028d805c894100c1b2
TiokengSamuel <samueledward40@gmail.com>
2020-11-05 17:44:36 +0100

    whole app

--- context-app/src/components/BookList.js
+++ context-app/src/components/BookList.js
@@ -8,9 +8,11 @@ export default class BookList extends Component {
         const theme = isLightTheme ? light : dark;
         return (
             <div className="book-list" style={{ color: theme.syntax, background: theme.bg}}>
-                <li style={{ background: theme.ui }}>The way of Kings</li>
-                <li style={{ background: theme.ui }}>The name of the wind</li>
-                <li style={{ background: theme.ui }}>The final empire</li>
+                <ul>
+                    <li style={{ background: theme.ui }}>The way of Kings</li>
+                    <li style={{ background: theme.ui }}>The name of the wind</li>
+                    <li style={{ background: theme.ui }}>The final empire</li>
+                </ul>
             </div>
         )
     }
