ff563979c92f99f799b2cf1e4a0354a6d1f35af1
TiokengSamuel <samueledward40@gmail.com>
2020-11-05 15:20:07 +0100

    context-app

---
+++ context-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css';
+import Login from './Login';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Showing the power of redux with facebook app</h1>
+      <h3>User is ...</h3>
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
---
+++ context-app/src/Login.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Login() {
+    
+    const loginToApp = () => {
+       
+    }
+
+    return (
+        <div>
+            <h1>I am Login component</h1>
+            <button onClick={loginToApp}>LOGIN</button>
+        </div>
+    )
+}
+
+export default Login
