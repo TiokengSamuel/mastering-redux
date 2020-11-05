cfc9f6bbbf4bc643c11eaf72d0188e46e1f43aae
TiokengSamuel <samueledward40@gmail.com>
2020-11-05 17:57:46 +0100

    consumer

--- context-app/src/components/Navbar.js
+++ context-app/src/components/Navbar.js
@@ -8,15 +8,19 @@ export default class Navbar extends Component {
         const { isLightTheme, light, dark} = this.context;
         const theme = isLightTheme ? light : dark;
         return (
-            <ThemeContext.Consumer>
-                <nav style = {{ background: theme.ui, color: theme.syntax }}>
-                    <h1>Context App</h1>
-                    <ul>
-                        <li>Home</li>
-                        <li>About</li>
-                        <li>Contact</li>
-                    </ul>
-                </nav>
+            <ThemeContext.Consumer>{(context) => {
+                return(
+                    <nav style = {{ background: theme.ui, color: theme.syntax }}>
+                        <h1>Context App</h1>
+                        <ul>
+                            <li>Home</li>
+                            <li>About</li>
+                            <li>Contact</li>
+                        </ul>
+                    </nav>
+                )
+            }}
+                
             </ThemeContext.Consumer>
         )
     }
