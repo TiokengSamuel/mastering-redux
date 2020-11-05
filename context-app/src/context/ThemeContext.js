a45198fea81b7ea36b3197fd2db22c0893e1e12f
TiokengSamuel <samueledward40@gmail.com>
2020-11-05 16:41:12 +0100

    theme

---
+++ context-app/src/context/ThemeContext.js
@@ -0,0 +1,16 @@
+import React, { createContext } from 'react'
+
+export const ThemeContext = createContext();
+
+
+ class ThemeContextProvider extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default ThemeContextProvider
