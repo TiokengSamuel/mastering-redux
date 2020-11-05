6de4744f8f0bf1de1732b3af829a48d7f7de9967
TiokengSamuel <samueledward40@gmail.com>
2020-11-05 16:44:34 +0100

    Themecontext

--- context-app/src/context/ThemeContext.js
+++ context-app/src/context/ThemeContext.js
@@ -4,12 +4,12 @@ export const ThemeContext = createContext();
 
 
  class ThemeContextProvider extends Component {
+    state = {
+        isLightTheme: true,
+        light: { syntax: '#555', ui: '#ddd', bg: '#eee'}
+    }
     render() {
-        return (
-            <div>
-                
-            </div>
-        )
+        return ();
     }
 }
 
