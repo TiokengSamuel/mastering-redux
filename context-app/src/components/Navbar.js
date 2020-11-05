46de61f9813beca6491d5c79635eda301d6be615
TiokengSamuel <samueledward40@gmail.com>
2020-11-05 17:24:08 +0100

    this.context

--- context-app/src/components/Navbar.js
+++ context-app/src/components/Navbar.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react'
+import { ThemeContext } from '../context/ThemeContext'
 
 export default class Navbar extends Component {
+    static contextType = ThemeContext;
     render() {
+        console.log(this.context);
+        const { isLightTheme, light, dark} = this.context;
         return (
+            
             <nav>
                 <h1>Context App</h1>
                 <ul>
