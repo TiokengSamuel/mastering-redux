c919420be08e8de4a410dfc5475c67f17ed1a414
TiokengSamuel <samueledward40@gmail.com>
2020-11-05 15:35:41 +0100

    bookList component

---
+++ context-app/src/components/BookList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class BookList extends Component {
+    render() {
+        return (
+            <div className="book-list">
+                <li>The way of Kings</li>
+                <li>The name of the wind</li>
+                <li>The final empire</li>
+            </div>
+        )
+    }
+}
