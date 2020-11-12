2f4251a8da277f0a7de9dabc5dc93c19f508c5af
TiokengSamuel <samueledward40@gmail.com>
2020-11-12 17:34:48 +0100

    songlist

---
+++ hooks-app/src/Components/SongList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const SongList = () => {
+    return (
+        <div className="song-list">
+            <ul>
+                <li>This wild darkness</li>
+                <li>Memory Gospel</li>
+            </ul>
+        </div>
+    )
+}
