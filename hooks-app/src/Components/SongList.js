53ad5949b3bab889c22a709ccec1ebcf5ce42819
TiokengSamuel <samueledward40@gmail.com>
2020-11-12 21:50:56 +0100

    song

--- hooks-app/src/Components/SongList.js
+++ hooks-app/src/Components/SongList.js
@@ -9,10 +9,14 @@ export const SongList = () => {
         { title: 'Memory Gospel', id: 2 },
         { title: 'This wild Darkness', id: 3 },
     ]);
+    const [age, setAge] = useState(20);
 
     const addSong = (title) => {
         setSongs([...songs, {title: title, id: uuid()}])
     }
+    useEffect(() => {
+        
+    })
     return (
         <div className="song-list">
             <ul>
@@ -21,6 +25,7 @@ export const SongList = () => {
                 })}
             </ul>
             <NewSongForm addSong = {addSong}/>
+            <button onClick = {() => setAge(age + 1)}>Add 1 to age: {age}</button>
         </div>
     )
 }
