2b3e562f9f8613464e13de8ecd67d1a9340fdc48
TiokengSamuel <samueledward40@gmail.com>
2020-11-12 17:35:44 +0100

    setup

--- hooks-app/src/App.js
+++ hooks-app/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { SongList } from './Components/SongList';
 
 function App() {
   return (
     <div className="App">
-     
+      <SongList/>
     </div>
   );
 }
