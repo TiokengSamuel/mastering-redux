382a2644c4c14c285d32160e99dd35130d17b02d
TiokengSamuel <samueledward40@gmail.com>
2020-11-12 18:17:49 +0100

    on change

--- hooks-app/src/Components/NewSongForm.js
+++ hooks-app/src/Components/NewSongForm.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react'
 
 export const NewSongForm = () => {
-    useState('');
+    const [title, setTitle] = useState('');
     return (
         <div>
             <form onSubmit={}>
                 <label>Song name: </label>
-                <input type="text" required onChange={e => {}}/>
+                <input type="text" required onChange={e => setTitle(e.target.value)}/>
                 <input type="submit" value="add song" />
             </form>
         </div>
