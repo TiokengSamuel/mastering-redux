4d8a69ca5f8762b857bf0e58154b7bc41432be0d
TiokengSamuel <samueledward40@gmail.com>
2020-11-14 12:42:18 +0100

    bookContext

--- context-app/src/context/BookContext.js
+++ context-app/src/context/BookContext.js
@@ -2,8 +2,8 @@ import React, { createContext, useState } from 'react'
 
 export const BookContext = createContext();
 
-const = (props) => {
-    useState([
+const BookContextProvider = (props) => {
+    const [books, setBooks] = useState([
         {title: 'The way of the King', id: 1},
         {title: 'The final empire', id: 2},
         {title: 'The hero of ages', id: 3}
