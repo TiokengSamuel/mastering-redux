1e48e8065f1418cac08dabb0405c9dd0cdb211a5
TiokengSamuel <samueledward40@gmail.com>
2020-11-14 11:30:30 +0100

    nav

--- context-app/src/components/Navbar.js
+++ context-app/src/components/Navbar.js
@@ -1,26 +1,34 @@
-import React, { Component } from 'react'
+import React, { Component, useContext } from 'react'
 import { ThemeContext } from '../context/ThemeContext'
+import { AuthContext } from '../context/Auth/Context'
 
 export default class Navbar extends Component {
     
     render() {
        
         return (
-            <ThemeContext.Consumer>{(context) => {
-                const { isLightTheme, light, dark} = context;
-                const theme = isLightTheme ? light : dark;
-                return(
-                    <nav style = {{ background: theme.ui, color: theme.syntax }}>
-                        <h1>Context App</h1>
-                        <ul>
-                            <li>Home</li>
-                            <li>About</li>
-                            <li>Contact</li>
-                        </ul>
-                    </nav>
-                )
-            }}   
-            </ThemeContext.Consumer>
+            <AuthContext.Consumer>{(authContext) => (
+                <ThemeContext.Consumer>{(context) => {
+                    const { isLightTheme, light, dark} = context;
+                    const theme = isLightTheme ? light : dark;
+                    return(
+                        <nav style = {{ background: theme.ui, color: theme.syntax }}>
+                            <h1>Context App</h1>
+                            <div onClick={toggleAuth}>
+                                { isAuthenticated ? 'Logged in' : 'Logged out'}
+                            </div>
+                            <ul>
+                                <li>Home</li>
+                                <li>About</li>
+                                <li>Contact</li>
+                            </ul>
+                        </nav>
+                    )
+                }}   
+                </ThemeContext.Consumer> 
+            )}
+            </AuthContext.Consumer>
+            
         )
     }
 }
