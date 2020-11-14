54aabbcbef51f8d002643647a74b18b6565b6cfa
TiokengSamuel <samueledward40@gmail.com>
2020-11-14 11:40:06 +0100

    context in navd

--- context-app/src/components/Navbar.js
+++ context-app/src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React, { Component, useContext } from 'react'
 import { ThemeContext } from '../context/ThemeContext'
-import { AuthContext } from '../context/Auth/Context'
+import { AuthContext } from '../context/AuthContext'
 
 export default class Navbar extends Component {
     
@@ -9,20 +9,11 @@ export default class Navbar extends Component {
         return (
             <AuthContext.Consumer>{(authContext) => (
                 <ThemeContext.Consumer>{(context) => {
+                    const { isAuthenticated, toggleAuth } = authContext;
                     const { isLightTheme, light, dark} = context;
                     const theme = isLightTheme ? light : dark;
                     return(
-                        <nav style = {{ background: theme.ui, color: theme.syntax }}>
-                            <h1>Context App</h1>
-                            <div onClick={toggleAuth}>
-                                { isAuthenticated ? 'Logged in' : 'Logged out'}
-                            </div>
-                            <ul>
-                                <li>Home</li>
-                                <li>About</li>
-                                <li>Contact</li>
-                            </ul>
-                        </nav>
+                       
                     )
                 }}   
                 </ThemeContext.Consumer> 
@@ -31,4 +22,21 @@ export default class Navbar extends Component {
             
         )
     }
+
+    const Navbar = () => {
+        return (
+            <nav style = {{ background: theme.ui, color: theme.syntax }}>
+            <h1>Context App</h1>
+            <div onClick={toggleAuth}>
+                { isAuthenticated ? 'Logged in' : 'Logged out'}
+            </div>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+        );
+    }
+    
 }
