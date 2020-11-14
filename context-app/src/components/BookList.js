212d26a2e1662da40498da3ec22bcbbd64c32a66
TiokengSamuel <samueledward40@gmail.com>
2020-11-14 10:30:02 +0100

    book

--- context-app/src/components/BookList.js
+++ context-app/src/components/BookList.js
@@ -17,3 +17,15 @@ export default class BookList extends Component {
         )
     }
 }
+
+const BookList = () => {
+    return (
+        <div className="book-list" style={{ color: theme.syntax, background: theme.bg}}>
+                <ul>
+                    <li style={{ background: theme.ui }}>The way of Kings</li>
+                    <li style={{ background: theme.ui }}>The name of the wind</li>
+                    <li style={{ background: theme.ui }}>The final empire</li>
+                </ul>
+            </div>
+    );
+}
