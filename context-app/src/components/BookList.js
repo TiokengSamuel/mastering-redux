2c439a7b2f3b8e26b67a9cafac5ce4a60a5ad7c0
TiokengSamuel <samueledward40@gmail.com>
2020-11-14 11:17:40 +0100

    book

--- context-app/src/components/BookList.js
+++ context-app/src/components/BookList.js
@@ -1,26 +1,10 @@
-import React, { Component } from 'react'
+import React, { useContext } from 'react'
 import { ThemeContext } from '../context/ThemeContext'
 
-export default class BookList extends Component {
-    static contextType = ThemeContext;
-    render() {
-        const { isLightTheme, light, dark } = this.context;
-        const theme = isLightTheme ? light : dark;
-        return (
-            <div className="book-list" style={{ color: theme.syntax, background: theme.bg}}>
-                <ul>
-                    <li style={{ background: theme.ui }}>The way of Kings</li>
-                    <li style={{ background: theme.ui }}>The name of the wind</li>
-                    <li style={{ background: theme.ui }}>The final empire</li>
-                </ul>
-            </div>
-        )
-    }
-}
 
 const BookList = () => {
-    const theme = isLightTheme ? light : dark;
     const { isLightTheme, light, dark} = useContext(ThemeContext);
+    const theme = isLightTheme ? light : dark;
     return (
         <div className="book-list" style={{ color: theme.syntax, background: theme.bg}}>
                 <ul>
@@ -31,3 +15,5 @@ const BookList = () => {
             </div>
     );
 }
+
+export default BookList;
