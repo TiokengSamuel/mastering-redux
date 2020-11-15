76dd141d93a33380d8220a8f7c09f2d8a0cfb0f8
TiokengSamuel <samueledward40@gmail.com>
2020-11-15 07:56:10 +0100

    bookcontext

--- book-list/src/contexts/BookContext.js
+++ book-list/src/contexts/BookContext.js
@@ -7,5 +7,9 @@ const BookContextProvider = (props) => {
        {title: 'Name of the wind', author: 'Patrick Rothfuss', id: 1},
        {title: 'The final empire', author: 'Brandon Sanderson', id: 2},
    
-    ])
+    ]);
+
+    const addBook = (title, author) => {
+        setBooks([...books, {title, author}])
+    }
 }
