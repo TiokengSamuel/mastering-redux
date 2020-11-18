029450c80c2822e1167ded1c4fe0fca8ec4f5b76
TiokengSamuel <samueledward40@gmail.com>
2020-11-18 12:43:42 +0100

    app

--- book-list/src/App.js
+++ book-list/src/App.js
@@ -3,12 +3,14 @@ import BookContextProvider from '../src/contexts/BookContext';
 import BookList from './components/BookList';
 import NavBar from './components/Navbar';
 
+
 function App() {
   return (
     <div className="app">
       <BookContextProvider>
         <NavBar/>
         <BookList/>
+        <NewBookForm/>
       </BookContextProvider>
     </div>
   );
