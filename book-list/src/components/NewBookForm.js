d9a95debfbc732d84241f753172d2b0e06999384
TiokengSamuel <samueledward40@gmail.com>
2020-11-18 14:35:07 +0100

    new

--- book-list/src/components/NewBookForm.js
+++ book-list/src/components/NewBookForm.js
@@ -8,9 +8,9 @@ const NewBookForm = () => {
     useContext(BookContext)
     return (
         <form>
-            <input type="text" placeholder="Book title" value={title}/>
+            <input type="text" placeholder="Book title" value={title}
             onChange={(e) => setTitle(e.target.value)}/>
-            <input type="text" placeholder="Book author" value={author}/>
+            <input type="text" placeholder="Book author" value={author}
             onChange={(e) => setAuthor(e.target.value)} />
         <input type="submit" value="Add book"/>
         </form>
