20e0b8651ac58f88d4e802821248f2d80c48e885
TiokengSamuel <samueledward40@gmail.com>
2020-11-18 10:20:23 +0100

    nav

--- book-list/src/App.js
+++ book-list/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import BookContextProvider from '../src/contexts/BookContext';
 import './App.css';
+import NavBar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <BookContextProvider>
-        
+        <NavBar/>
       </BookContextProvider>
     </div>
   );
