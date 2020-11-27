ee26be5d6234cf4db9031b0d724c1ea183c2dd6f
TiokengSamuel <samueledward40@gmail.com>
2020-11-27 07:36:09 +0100

    dispatch

--- book-list/src/contexts/BookContext.js
+++ book-list/src/contexts/BookContext.js
@@ -5,19 +5,8 @@ import { bookReducer } from '../reducers/bookreducer';
 export const BookContext = createContext();
 
 const BookContextProvider = (props) => {
-   const [books, dispatch] = useReducer(bookReducer, [][
-       
-   
-    ]);
+   const [books, dispatch] = useReducer(bookReducer, []);
 
-
-    const addBook = (title, author) => {
-        setBooks([...books, {title, author, id: uuid()}])
-    }
-
-    const removeBook = (id) => {
-        setBooks(books.filter(book => book.id !== id));
-    }
     return(
         <BookContext.Provider value={{books, addBook, removeBook}}>
             { props.children }
