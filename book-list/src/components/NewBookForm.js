ffabf8710279c6e8f5a2126b6910ca0a7d4297bf
TiokengSamuel <samueledward40@gmail.com>
2020-11-27 08:00:28 +0100

    newbook

--- book-list/src/components/NewBookForm.js
+++ book-list/src/components/NewBookForm.js
@@ -2,7 +2,7 @@ import React, {useContext, useState } from 'react'
 import {BookContext} from '../contexts/BookContext'
 
 const NewBookForm = () => {
-    const { addBook } = useContext(BookContext);
+    const { dispatch } = useContext(BookContext);
     const [title, setTitle] = useState('');
     const [author, setAuthor] = useState('');
     const handleSubmit = (e) => {
